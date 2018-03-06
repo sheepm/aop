6baece2d07aff6981d50e292743c06b3dc927ff1
paomo <redswing@foxmail.com>
2018-03-06 20:31:04 +0800

    添加  增加字段

--- libJava/src/main/java/sheepm/libjava/demo/AddField.java
+++ libJava/src/main/java/sheepm/libjava/demo/AddField.java
@@ -1,7 +1,15 @@
 package sheepm.libjava.demo;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import sheepm.libjava.asm.ClassReader;
 import sheepm.libjava.asm.ClassVisitor;
+import sheepm.libjava.asm.ClassWriter;
 import sheepm.libjava.asm.FieldVisitor;
+import sheepm.libjava.asm.Opcodes;
+import sheepm.libjava.asm.Type;
 
 import static sheepm.libjava.asm.Opcodes.ASM5;
 
@@ -9,7 +17,7 @@ import static sheepm.libjava.asm.Opcodes.ASM5;
  * Created by paomo on 2018/3/6.
  */
 
-public class AddField extends ClassVisitor{
+public class AddField extends ClassVisitor {
 
     private String name;
     private int access;
@@ -18,7 +26,7 @@ public class AddField extends ClassVisitor{
 
     private boolean duplicate;
 
-    public AddField(ClassVisitor cv,String name,int access,String desc,Object value) {
+    public AddField(ClassVisitor cv, String name, int access, String desc, Object value) {
         super(ASM5, cv);
         this.name = name;
         this.access = access;
@@ -28,7 +36,7 @@ public class AddField extends ClassVisitor{
 
     @Override
     public FieldVisitor visitField(int access, String name, String desc, String signature, Object value) {
-        if (this.name.equals(name)){
+        if (this.name.equals(name)) {
             duplicate = true;
         }
         return super.visitField(access, name, desc, signature, value);
@@ -36,12 +44,29 @@ public class AddField extends ClassVisitor{
 
     @Override
     public void visitEnd() {
-        if (!duplicate){
+        if (!duplicate) {
             FieldVisitor fv = super.visitField(access, name, desc, null, value);
-            if (fv != null){
+            if (fv != null) {
                 fv.visitEnd();
             }
         }
         super.visitEnd();
+    }
+
+    public static void main(String[] args) throws Exception {
+        String output = System.getProperty("user.dir") + "/libjava/output";
+        String classDir = System.getProperty("user.dir") + "/libjava/output/MainActivity.class";
+        ClassReader classReader = new ClassReader(new FileInputStream(classDir));
+        ClassWriter classWriter = new ClassWriter(classReader, ClassWriter.COMPUTE_MAXS);
+        ClassVisitor addField = new AddField(classWriter,
+                "field",
+                Opcodes.ACC_PUBLIC + Opcodes.ACC_STATIC + Opcodes.ACC_FINAL,
+                Type.getDescriptor(String.class),
+                "value"
+        );
+        classReader.accept(addField, ClassReader.EXPAND_FRAMES);
+        byte[] newClass = classWriter.toByteArray();
+        File newFile = new File(output, "MainActivity1.class");
+        new FileOutputStream(newFile).write(newClass);
     }
 }
